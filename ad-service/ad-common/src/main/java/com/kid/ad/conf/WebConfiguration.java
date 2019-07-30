6e0d1fd22541885ae5ab752ab7883ef8ab6d9391
996kid <1069554723@qq.com>
2019-07-30 19:46:57 +0800

    in progress

--- ad-service/ad-common/src/main/java/com/kid/ad/conf/WebConfiguration.java
+++ ad-service/ad-common/src/main/java/com/kid/ad/conf/WebConfiguration.java
@@ -1,8 +1,13 @@
 package com.kid.ad.conf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,9 +20,54 @@ import java.util.List;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    /** 配置消息转换器
+     * @param converters
+     */
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.clear();
         converters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+    /**
+     * 匹配路由请求规则
+     * @param configurer
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+
+    }
+
+    /**
+     * 注册自定义的fomatter convert
+     * @param registry
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+    }
+
+    /** 添加静态资源处理器
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+
+    /** 配置自定义视图控制器
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+    }
+
+    /** 添加自定义方法参数处理器
+     * @param resolvers
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+
     }
 }
